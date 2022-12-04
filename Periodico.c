f6a07b9718d45c3ad9e19338c68beed097b597d2
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2022-12-04 13:14:50 +0100

    Update commit
    Update

--- Periodico.c
+++ Periodico.c
@@ -13,32 +13,33 @@ if( strcmp(edit,"EL MUNDO")== 0){
     printf("El Periodico The Wall Street Journal es confiable,pero revise en otros\n",edit);
 }
 
-    else if ( strcmp(edit,"EL PAÍS")== 0){
-    printf("El periodico EL PAÍS (Postura Política: Centroizquierda​​)​,es confiable,revise otros periodicos para mejor verificación.\n",edit);
+    else if ( strcmp(edit,"EL PAIS")== 0){
+    printf("El periodico EL PAIS (Postura Politica: Centroizquierda​​)​,es confiable,revise otros periodicos para mejor verificacion.\n",edit);
     }
     else if( strcmp(edit,"ABC")== 0){
-        printf("El periodico ABC (Ideologia Política:Socioliberalismo), es confiable,revise otros periodicos para mejor verificación.\n",edit);
+        printf("El periodico ABC (Ideologia Politica:Socioliberalismo), es confiable,revise otros periodicos para mejor verificacion.\n",edit);
     }
     else if(strcmp(edit, "La Vanguardia")== 0){
-        printf("El periodico La Vanguardia (Ideologia Política: Centrismo, liberalismo, monarquismo, catalanismo), es confiable,revise otros periodicos para mejor verificación.\n",edit);
+        printf("El periodico La Vanguardia (Ideologia Politica: Centrismo, liberalismo, monarquismo, catalanismo), es confiable,revise otros periodicos para mejor verificacion.\n",edit);
     }
 else if(strcmp(edit, "El Espanol")== 0){
-        printf("El periodico El Español(Ideologia Política: Socioliberalista), es confiable,revise otros periodicos para mejor verificación.\n",edit);
+        printf("El periodico El Español(Ideologia Politica: Socioliberalista), es confiable,revise otros periodicos para mejor verificacion.\n",edit);
     }
     else if(strcmp(edit, "La Razon")== 0){
-        printf("El periodico La Razon (Ideologia Política: Conservadurismo,Monarquismo), es confiable,revise otros periodicos para mejor verificación.\n",edit);
+        printf("El periodico La Razon (Ideologia Politica: Conservadurismo,Monarquismo), es confiable,revise otros periodicos para mejor verificacion.\n",edit);
     }
      else if(strcmp(edit, "Ok Diario")== 0){
-        printf("El periodico Ok Diario (Ideologia Política: Neoliberalismo​Nacionalismo), no es muy confiable,revise otros periodicos para mejor verificación.\n",edit);
+        printf("El periodico Ok Diario (Ideologia Politica: Neoliberalismo,​Nacionalismo), no es muy confiable,revise otros periodicos para mejor verificacion.\n",edit);
     }
 
 else if(strcmp(edit, "Antena 3")== 0){
-        printf("El periodico Antena 3 (Ideologia Política: El PP),es confiable,revise otros periodicos para mejor verificación.\n",edit);
+        printf("El periodico Antena 3 (Ideologia Politica: El PP),es confiable,revise otros periodicos para mejor verificacion.\n",edit);
 
     }
     else{
     printf("Ese periodico no es confiable o poco conocido(Contacte con soporte)");
 }
+system("pause");
 return 0;
 
 }
